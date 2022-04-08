38a1bf2240b48b9c644ff4b524711ac93f5a92de
HunterPlus <zlw08140957@gmail.com>
2022-04-08 15:32:37 +0800

    Update prodcons.c

--- prodcons.c
+++ prodcons.c
@@ -4,26 +4,26 @@
 
 #define min(a,b) (((a)<(b))?(a):(b))
 #define MAXNITEMS	1000000
-#define MAXNTHREAD	50
-void Thread_create(pthread_t *, pthread_attr_t *, void *(*)(void *), (void *));
+#define MAXNTHREADS	50
+void Thread_create(pthread_t *, pthread_attr_t *, void *(*)(void *), void *);
 void *produce(void *), *consume(void *);
 
 int nitems;	/* read only by produce and consume */
 struct {
-	pthread_mutex	mutex;
+	pthread_mutex_t	mutex;
 	int		buff[MAXNITEMS];
 	int		nput;
 	int		nval;
 } shared = {
 	PTHREAD_MUTEX_INITIALIZER
-}；
+};
 
 int main(int argc, char *argv[])
 {
 	int i, nthreads, count[MAXNTHREADS];
 	pthread_t tid_produce[MAXNTHREADS], tid_consume;
 	
-	if (argc ! = 3) {
+	if (argc != 3) {
 		fprintf(stderr, "arguments error.\n");
 		exit(1);
 	}
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 		Pthread_create(&tid_produce[i], NULL, produce, &count[i]);		
 	}
 	for (i = 0; i < nthreads; i++) {
-		pthreads_join(tid_produce[i], NULL);
+		pthread_join(tid_produce[i], NULL);
 		printf("count[%d] = %d\n", i, count[i]);
 	}
 	
@@ -62,13 +62,13 @@ void *consume(void *arg)
 	
 	for (int i = 0; i < nitems; i++)
 		if (shared.buff[i] != i)
-			printf("buffer[%d] = %d\n", i, shared.buffer[i]);
+			printf("buffer[%d] = %d\n", i, shared.buff[i]);
 	return NULL;
 }
-void Pthread_create(pthread_t *thread, pthread_attr_t *attr, void *(*start)(void *), (void *)arg)
+void Pthread_create(pthread_t *thread, pthread_attr_t *attr, void *(*start)(void *), void *arg)
 {
-	if (pthread_create(thread, NULL, start, arg) != 0) {
+	if (pthread_create(thread, attr, start, arg) != 0) {
 		fprintf(stderr, "thread create error.\n");
-		exit;
+		exit(1);
 	}
 }
