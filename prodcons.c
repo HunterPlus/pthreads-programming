049acc8192353a795e01df338ae06142198b8d73
HunterPlus <zlw08140957@gmail.com>
2022-04-08 14:35:48 +0800

    Update prodcons.c

--- prodcons.c
+++ prodcons.c
@@ -7,3 +7,30 @@
 #define MAXNTHREAD	50
 void Thread_create(pthread_t *, pthread_attr_t *, void *(*)(void *), (void *));
 void *produce(void *), *consume(void *);
+
+int nitems;	/* read only by produce and consume */
+struct {
+	pthread_mutex	mutex;
+	int		buff[MAXNITEMS];
+	int		nput;
+	int		nval;
+} shared = {
+	PTHREAD_MUTEX_INITIALIZER
+}；
+
+int main(int argc, char *argv[])
+{
+	
+}
+void *produce(void *arg)
+{
+	
+}
+void *consume(void *arg)
+{
+	
+}
+void Pthread_create(pthread_t *thread, pthread_attr_t *attr, void *(*start)(void *), (void *)arg)
+{
+	
+}
