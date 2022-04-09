57aadebcd93157db51d96e71e97b7e229867aa22
HunterPlus <zlw08140957@gmail.com>
2022-04-09 10:29:11 +0800

    Update prodcons2.c

--- prodcon/prodcons2.c
+++ prodcon/prodcons2.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define min(a,b) (((a)<(b))?(a):(b))
 #define MAXNITEMS	1000000
 #define MAXNTHREADS	50
 
+void Pthread_create(pthread_t *, pthread_attr_t *, void *(*)(void *), void *);
+void *produce(void *), *consume(void *);
 int nitems;
 int buff[MAXNITEMS];
 struct {
@@ -23,3 +26,28 @@ struct {
 	PTHREAD_MUTEX_INITIALIZAER, PTHREAD_COND_INITIALIZER
 };
 
+int main(int argc, char *argv[])
+{
+	int i, nthreads, count[MAXNTHREADS];
+	pthread_t tid_produce[MAXNTHREADS], tid_consume;
+	
+	if (argc != 3) {
+		fprintf(stderr, "arguments error.\n");
+		exit(1);
+	}
+	nitems = min(atoi(argv[1]), MAXNITEMS);
+	nthreads = min(atoi(argv[2]), MAXNTHREADS);
+	for (i = 0; i < nthreads; i++) {
+		count[i] = 0;
+		Pthread_create(&tid_produce[i], NULL, produce, &count[i]);		
+	}
+	Pthread_create(&tid_consume, NULL, consume, NULL);
+	
+	/* wait for all produces and consume thread */
+	for (i = 0; i < nthreads; i++) {
+		pthread_join(tid_produce[i], NULL);
+		printf("count[%d] = %d\n", i, count[i]);
+	}	
+	pthread_join(tid_consume, NULL);
+	return 0;	
+}
