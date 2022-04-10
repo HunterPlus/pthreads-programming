6b545db3327cedb7b67399cb83648586bf02d56f
HunterPlus <zlw08140957@gmail.com>
2022-04-10 14:04:44 +0800

    Update rwlock.h

--- readwritelock/rwlock.h
+++ readwritelock/rwlock.h
@@ -7,3 +7,17 @@ typedef struct {
 	int 	rw_nwaitwriters;
 	int	rw_refcount;	/* -1 if writer has the lock, else readers holding the lock */
 } pthread_rwlock_t;
+
+#define RW_MAGIC	0x19283746
+
+#define PTHREAD_RWLOCK_INITIALIZER	{ PTHREAD_MUTEX_INITIALIZER, 	\
+		PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER,	\
+		RW_MAGIC, 	0,	0,	0 }		
+typedef int pthread_rwlockattr_t;	/* dummy, not supported */
+
+int pthread_rwlock_init(pthread_rwlock_t *, pthread_rwlockattr_t *);
+int pthread_rwlock_destroy(pthread_rwlock_t *);
+int pthread_rwlock_rdlock(pthread_rwlock_t *);
+int pthread_rwlock_rdunlock(pthread_rwlock_t *);
+int pthread_rwlock_wrlock(pthread_rwlock_t *);
+int pthread_rwlock_wrunlock(pthread_rwlock_t *);
