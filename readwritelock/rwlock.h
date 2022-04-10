4da4bf499c1b545e2eaa14a2271ac1da2bafcc95
HunterPlus <zlw08140957@gmail.com>
2022-04-10 19:00:22 +0800

    Update rwlock.h

--- readwritelock/rwlock.h
+++ readwritelock/rwlock.h
@@ -18,7 +18,9 @@ typedef int pthread_rwlockattr_t;	/* dummy, not supported */
 int pthread_rwlock_init(pthread_rwlock_t *, pthread_rwlockattr_t *);
 int pthread_rwlock_destroy(pthread_rwlock_t *);
 int pthread_rwlock_rdlock(pthread_rwlock_t *);
+int pthread_rwlock_tryrdlock(pthread_rwlock_t *);
 int pthread_rwlock_wrlock(pthread_rwlock_t *);
+int pthread_rwlock_trywrlock(pthread_rwlock_t *);
 int pthread_rwlock_unlock(pthread_rwlock_t *);
 
 
