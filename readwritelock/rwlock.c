e38d28e5f3691f18345d2be4c5e0b6731969591f
HunterPlus <zlw08140957@gmail.com>
2022-04-10 14:39:03 +0800

    Update rwlock.c

--- readwritelock/rwlock.c
+++ readwritelock/rwlock.c
@@ -21,9 +21,24 @@ int pthread_rwlock_init(pthread_rwlock_t *rw, pthread_rwlockattr_t *attr)
 	return 0;
 	
 err3:
-	pthread_cond_destroy(rw->rw_condreaders);
+	pthread_cond_destroy(&rw->rw_condreaders);
 err2:
-	pthread_mutex_destroy(rw->rw_mutex);
+	pthread_mutex_destroy(&rw->rw_mutex);
 err1:
 	return result;		/* an errno value */
+}
+
+int pthread_rwlock_destroy(pthread_rwlock_t *rw)
+{
+	if (rw->rw_magic != RW_MAGIC)
+		return (EINVAL);
+	if (rw->rw_refcount != 0 || rw->rw_nwaitreaders != 0 || rw->rw_nwaitwriters != 0)
+		return (EBUSY);
+	
+	pthread_mutex_destroy(&rw->rw_mutex);
+	pthread_cond_destroy(&rw->rw_condreaders);
+	pthread_cond_destroy(&rw->rw_condwriters);
+	rw->rw_magic = 0;
+	
+	return 0;
 }
