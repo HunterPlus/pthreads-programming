41efc82a8cd61ff27669f14d61a38f5bbe5d7593
HunterPlus <zlw08140957@gmail.com>
2022-04-10 19:06:39 +0800

    Update rwlock.c

--- readwritelock/rwlock.c
+++ readwritelock/rwlock.c
@@ -65,3 +65,22 @@ int pthread_rwlock_rdlock(pthread_rwlock_t *rw)
 	pthread_mutex_unlock(&rw->rw_mutex);
 	return result;
 }
+
+int pthread_rwlock_tryrdlock(pthread_rwlock_t *rw)
+{
+	int	result;
+	
+	if (rw->rw_magic != RW_MAGIC)
+		return (EINVAL);
+	
+	if ((result = pthread_mutex_lock(&rw->rw_mutex)) != 0)
+		return (result);
+	
+	if (rw->rw_refcount < 0 || rw->rw_nwaitrwiters > 0)
+		result = EBUSY;
+	else
+		rw->rw_refcount++;
+	pthread_mutex_unlock(&rw->rw_mutex);
+	
+	return (result);
+}
