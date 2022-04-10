3914804551c37ee895afbfedf4581eb1a88f313f
HunterPlus <zlw08140957@gmail.com>
2022-04-10 14:40:43 +0800

    Update rwlock.c

--- readwritelock/rwlock.c
+++ readwritelock/rwlock.c
@@ -7,11 +7,11 @@ int pthread_rwlock_init(pthread_rwlock_t *rw, pthread_rwlockattr_t *attr)
 	if (attr != NULL)
 		return (EINVAL);	/* not supported */
 	
-	if ((result = pthread_mutex_init(rw->rw_mutex, NULL)) != 0)
+	if ((result = pthread_mutex_init(&rw->rw_mutex, NULL)) != 0)
 		goto err1;
-	if ((result = pthread_cond_init(rw->rw_condreaders, NULL)) != 0)
+	if ((result = pthread_cond_init(&rw->rw_condreaders, NULL)) != 0)
 		goto err2;
-	if ((result = pthread_cond_init(rw->rw_condwriters, NULL)) != 0)
+	if ((result = pthread_cond_init(&rw->rw_condwriters, NULL)) != 0)
 		goto err3;
 	rw->rw_nwaitreaders = 0;
 	rw->rw_nwaitwriters = 0;
