e6ddb5ecdc1c85e508c3b50179af45ea96cf91e3
HunterPlus <zlw08140957@gmail.com>
2022-04-11 19:52:27 +0800

    Update prodcons2.c

--- semaphore/prodcons2.c
+++ semaphore/prodcons2.c
@@ -8,6 +8,8 @@
 #define NBUFF		10
 #define MAXNTHREADS	100
 
+#define min(a,b)	(((a) < (b)) ? (a) : (b))
+
 int 	nitems, nproducers, nconsumers;
 
 struct {
@@ -23,4 +25,29 @@ void *produce(void *), *consume(void *);
 
 int main(int argc, char *argv[])
 {
+	int	i, prodcount[MAXNTHREADS], conscount[MAXNTHREADS];
+	pthread_t tid_produce[MAXNTHREADS], tid_consume[MAXNTHREADS];
+	
+	if (argc ! = 4) {
+		fprintf(stderr, "usage: prodcons2 <#items> <#producers> <#consumers>\n");
+		exit(1);
+	}
+	nitems = atoi(argv[1]);
+	nproducers = min(atoi(argv[2]), MAXNTHREADS);
+	nconsumers = min(atoi(argv[3]), MAXNTHREADS);
+	
+	sem_init(&shared.mutex, 0, 1);
+	sem_init(&shared.nempty, 0, NBUFF);
+	sem_init(&shared.nstored, 0, 0);
+	
+	for (i = 0; i < nproducers; i++) {
+		prodcount[i] = 0;
+		pthread_create(&tid_produce[i], NULL, produce, &prodcount[i]);
+	}
+	for (i = 0; i < nconsumers; i++) {
+		consount[i] = 0;
+		pthread_create(&tid_consume[i], NULL, consume, &conscount[i]);
+	}
+	
+	
 }
