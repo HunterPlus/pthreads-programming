b543c6fc79017b01451746bbb0d2688ba8b7c83f
HunterPlus <zlw08140957@gmail.com>
2022-04-11 17:35:48 +0800

    Create prodcons1.c

---
+++ semaphore/prodcons1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define	NBUFF		10
+#define	MAXNTHREADS	100
+#define min(a,b)	(((a) < (b)) ? (a) : (b))
+
+int 	nitems, nproducer;
+struct {
+	int	buff[NBUFF];
+	int	nput;
+	int	nvalue;
+	sem_t	mutex, nempty, nstored;
+} shared;
+
+void	*produce(void *), *consume(void *);
+
+int main(int argc, char *argv[])
+{
+	int	i, count[MAXNTHREADS];
+	pthread_t tid_produce[MAXNTHREADS], tid_consume;
+	
+	if (argc != 3) {
+		fprintf(stderr, "usage prodcons1 <#items> <#producers>\n");
+		exit(1);
+	}
+	nitems = atoi(argv[1]);
+	nproducers = min(atoi([argv[2]), MAXNTHREADS);
+}
