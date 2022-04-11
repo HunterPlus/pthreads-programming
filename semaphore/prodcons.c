3cfbd75291445d1f81f28e95fd7b41474ca8a73d
HunterPlus <zlw08140957@gmail.com>
2022-04-11 14:48:21 +0800

    Update prodcons.c

--- semaphore/prodcons.c
+++ semaphore/prodcons.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -14,4 +15,26 @@ void *produce(void *), *consume(void *);
 
 int main(int argc, char *argv[])
 {
+	pthread_t tid_produce, tid_consume;
+	
+	if (argc != 2) {
+		fprintf(stderr, "usage: prodcons <# items>");
+		exit(1);
+	}
+	nitems = atoi(argv[1]);
+	
+	sem_init(&shared.mutex, 0, 1);
+	sem_init(&shared.nempty, 0, NBUFF);
+	sem_init(&shared.nstored, 0, 0);
+	
+	pthread_create(&tid_produce, NULL, produce, NULL);
+	pthread_create(&tie_consume, NULL, consume, NULL);
+	
+	pthread_join(&tid_produce, NULL);
+	pthread_join(&tid_consume, NULL);
+	
+	sem_destroy(&shared.mutex);
+	sem_destroy(&shared.nempty);
+	sem_destroy(&shared.nstored);
+	return 0;
 }
