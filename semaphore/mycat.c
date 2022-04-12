43a045c71f65e9c211f390fa2403cddb016ce3b7
HunterPlus <zlw08140957@gmail.com>
2022-04-12 10:25:13 +0800

    Update and rename prodcons3.c to mycat.c

---
+++ semaphore/mycat.c
@@ -0,0 +1,49 @@
+/* Multiple Buffers */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define	NBUFF	8
+
+struct {				/* data shared by producer and consumer */
+	struct {
+		char data[BUFFSIZ];	/* a buffer */
+		ssize_t	n;		/* count of #bytes in the buffer */
+	} buff[NBUFF];			/* NBUFF of these buffers/counts */
+	sem_t	mutex, nempty, nstored;	/* semaphores, not pointers */
+} shared;
+
+int	fd;				/* input file to copy to stdout */
+void	*produce(void *), *consume(void *);
+
+int main(int argc, char *argv[])
+{
+	pthread_t tid_procude, tid_consume;
+	
+	if (argc != 2) {
+		fprintf(stderr, "usage: mycat <pathname>");
+		exit(1);
+	}
+	if ((fd = open(argv[1], O_RDONLY)) == -1) {
+		fprintf(stderr, "file open error.\n");
+		exit(1);
+	}
+	
+	sem_init(&shared.mutex, 0, 1);
+	sem_init(&shared.nempty, 0, NBUFF);
+	sem_init(&shared.ntored, 0, 0);
+	
+	pthread_create(&tid_produce, NULL, produce, NULL);	/* read thread */
+	pthread_create(&tid_consume, NULL, consume, NULL);	/* write thread */
+	
+	pthread_join(tid_produce, NULL);
+	pthread_join(tid_consume, NULL);
+	
+	sem_destroy(&shared.mutex);
+	sem_destroy(&shared.nempty);
+	sem_destroy(&shared.nstored);
+	
+	return 0;
+}
