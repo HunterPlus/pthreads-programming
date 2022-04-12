05255bcb2854f433f8e151e61d98020400f486b0
HunterPlus <zlw08140957@gmail.com>
2022-04-12 10:10:41 +0800

    Update prodcons3.c

--- semaphore/prodcons3.c
+++ semaphore/prodcons3.c
@@ -5,3 +5,13 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+#define	NBUFF	8
+
+struct {				/* data shared by producer and consumer */
+	struct {
+		char data[BUFFSIZ];	/* a buffer */
+		ssize_t	n;		/* count of #bytes in the buffer */
+	} buff[NBUFF];			/* NBUFF of these buffers/counts */
+	sem_t	mutex, nempty, nstored;	/* semaphores, not pointers */
+} shared;
+
