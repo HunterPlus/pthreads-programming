7c4e892236601a30189bf95a333ba53445b7d1c6
HunterPlus <zlw08140957@gmail.com>
2022-04-20 10:03:37 +0800

    Update foo.c

--- mutex/foo.c
+++ mutex/foo.c
@@ -5,13 +5,13 @@ struct foo {
 	int	count;
 	pthread_mutex_t mutex;
 	int	id;
-	/* ... more stuff hear ... */
-	if ((fp = malloc(sizeof(struct foo))) != NULL) {
+	/* ... more stuff hear ... */	
 };
 struct foo *foo_alloc(int id)	/* alloc the object */
 {
 	struct foo *fp;
 	
+	if ((fp = malloc(sizeof(struct foo))) != NULL) {
 		fp->count = 1;
 		fp->id = id;
 		if (pthread_mutex_init(&fp->mutex, NULL) != 0) {
