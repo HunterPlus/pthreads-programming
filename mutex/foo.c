2658cc657fcde370f3d34c1f5eb1f4b69ac60362
HunterPlus <zlw08140957@gmail.com>
2022-04-20 10:02:22 +0800

    Update foo.c

--- mutex/foo.c
+++ mutex/foo.c
@@ -1,2 +1,40 @@
 #include <stdlib.h>
 #include <pthread.h>
+
+struct foo {
+	int	count;
+	pthread_mutex_t mutex;
+	int	id;
+	/* ... more stuff hear ... */
+	if ((fp = malloc(sizeof(struct foo))) != NULL) {
+};
+struct foo *foo_alloc(int id)	/* alloc the object */
+{
+	struct foo *fp;
+	
+		fp->count = 1;
+		fp->id = id;
+		if (pthread_mutex_init(&fp->mutex, NULL) != 0) {
+			free(fp);
+			return (NULL);
+		}
+		/* ... continue initialization ... */
+	}
+	return (fp);
+}
+void foo_hold(struct foo *fp)	/* add a reference to the object */
+{
+	pthread_mutex_lock(&fp->mutex);
+	fp->count++;
+	pthread_mutex_unlock(&fp->mutex);
+}
+void foo_rele(struct foo *fp)	/* release a reference to the object */
+{
+	pthread_mutex_lock(&fp->mutex);
+	if (--fp->count == 0) {		/* last reference */
+		pthread_mutex_unlock(&fp->mutex);
+		pthread_mutex_destroy(&fp->mutex);
+		free(fp);
+	} else 
+		pthread_mutex_unlock(&fp->mutex);
+}
