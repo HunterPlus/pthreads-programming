d751f625a8c3d3692fb2a4798a5a5ba3c58e56e6
HunterPlus <zlw08140957@gmail.com>
2022-04-20 13:59:27 +0800

    Update hlistlock.c

--- mutex/hlistlock.c
+++ mutex/hlistlock.c
@@ -3,3 +3,14 @@
 
 #define NHASH	29
 #define HASH(id)	(((unsigned long)id)%NHASH)
+
+struct foo *fh[NHASH];
+pthread_mutex_t hashlock = PTHREAD_MUTEX_INITIALIZER;
+
+struct foo {
+	int	count;		/* protected by hashlock */
+	pthread_mutex_t	lock;
+	int	id;
+	struct foo *next;	/* protected by hashlock */
+	/* ... more stuff here ... */
+};
